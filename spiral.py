7c69656164df7a0d7d1b6f57dbc5ccb1e1699a99
Vinayak Sahal <32996115+vsahal@users.noreply.github.com>
2019-02-01 19:39:28 -0600

    Update spiral.py
    updated

--- spiral.py
+++ spiral.py
@@ -1,6 +1,6 @@
 #  File: Spiral.py
 
-#  Description:
+#  Description: Algorithm that finds sum of surrounding numbers of a given int
 
 #  Student Name: Vinayak Sahal
 
@@ -15,68 +15,86 @@
 #  Date Last Modified: 2/1/2019
 
 
-def spiral():
 
-  spiralFile = open('spiral.txt', 'r')
-  n = int((spiralFile.readline()))
-  n = n + 2
+# spiral function that makes spiral with one paramter 'n'
+def spiral(n):
 
-  matrix = [[0] * (n) for i in range(n)]
+    n = n + 2
+    matrix = [[0] * (n) for i in range(n)]
 
-  rowStart = 1
-  rowEnd = n - 2
-  colStart = 1
-  colEnd = n - 2
+    # setting variables up
+    rowStart = 1
+    rowEnd = n - 2
+    colStart = 1
+    colEnd = n - 2
 
-  beginNum = (n - 2) * (n - 2)
+    beginNum = (n - 2) * (n - 2)
 
-  while (rowStart <= rowEnd and colStart <= colEnd):
+    # while loop to make sure conditions are met so it can continue
+    while (rowStart <= rowEnd and colStart <= colEnd):
 
-    for col in range(colStart, colEnd + 1):
-      matrix[rowStart][col] = beginNum
-      beginNum -= 1
+        # goes and makes one column on one side of square
+        for col in range(colStart, colEnd + 1):
+            matrix[rowStart][col] = beginNum
+            beginNum -= 1
 
-    rowStart += 1
+        rowStart += 1
 
-    for row in range(rowStart, rowEnd + 1):
-      matrix[row][colEnd] = beginNum
-      beginNum -= 1
+        # goes and makes one column on one side of square
+        for row in range(rowStart, rowEnd + 1):
+            matrix[row][colEnd] = beginNum
+            beginNum -= 1
 
-    colEnd -= 1
+        colEnd -= 1
 
-    for col in range(colEnd, colStart - 1, -1):
-      matrix[rowEnd][col] = beginNum
-      beginNum -= 1
+        # goes and makes one column on one side of square
+        for col in range(colEnd, colStart - 1, -1):
+            matrix[rowEnd][col] = beginNum
+            beginNum -= 1
 
-    rowEnd -= 1
+        rowEnd -= 1
 
-    for row in range(rowEnd, rowStart - 1, -1):
-      matrix[row][colStart] = beginNum
-      beginNum -= 1
+        # goes and makes one column on one side of square
+        for row in range(rowEnd, rowStart - 1, -1):
+            matrix[row][colStart] = beginNum
+            beginNum -= 1
 
-    colStart += 1
+        colStart += 1
 
-  return matrix
+    return(matrix)
+
+# sums all values around n
+def sumNum(matrix, n):
+
+    count = 0
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if matrix[i][j] == n:
+                count += matrix[i][j + 1]
+                count += matrix[i][j - 1]
+                count += matrix[i - 1][j]
+                count += matrix[i - 1][j - 1]
+                count += matrix[i - 1][j + 1]
+                count += matrix[i + 1][j]
+                count += matrix[i + 1][j + 1]
+                count += matrix[i + 1][j - 1]
+
+    return(count)
 
 
-matrix = spiral()
 
-for row in matrix:
-  print(row)
+# main calls the function and reads the text file
+def main():
+    # opening text file to be read
+    inSpiral = open('spiral.txt', 'r')
+    n = int((inSpiral.readline()))
+
+    matrix = spiral(n)
+
+    # printing sum of surrounding nums
+    for line in inSpiral:
+        print(sumNum(matrix, int(line)))
 
 
-count = 0
-
-for i in range(len(matrix)):
-  for j in range(len(matrix[0])):
-    if matrix[i][j] == :
-      count += matrix[i][j + 1]
-      count += matrix[i][j - 1]
-      count += matrix[i - 1][j]
-      count += matrix[i - 1][j - 1]
-      count += matrix[i - 1][j + 1]
-      count += matrix[i + 1][j]
-      count += matrix[i + 1][j + 1]
-      count += matrix[i + 1][j - 1]
-
-print(count)
+main()
