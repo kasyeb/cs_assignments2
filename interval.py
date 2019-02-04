e93ffc7c4c3c22fb919a9483e9fb465d00e35504
Vinayak Sahal <32996115+vsahal@users.noreply.github.com>
2019-02-03 18:18:45 -0600

    Create interval.py
    code that finds over lapping intervals

---
+++ interval.py
@@ -0,0 +1,4 @@
+# using text file append tuples to a list
+a = tuple([(14,17),(-8,-5),(26,29), (-20,-15),(12,15),(2,3),(-10,-7),(25,30),(2,4)])
+a = sorted(a, reverse=False)
+# a is now sorted in ascending order
