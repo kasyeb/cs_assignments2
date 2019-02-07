87e7285bbf78fe86823898f68bd15802e0aefd36
Vinayak Sahal <32996115+vsahal@users.noreply.github.com>
2019-02-07 14:07:45 -0600

    Update cryptography.py
    completed encrypt part

--- cryptography.py
+++ cryptography.py
@@ -1,1 +1,31 @@
-# new assignment
+
+#ENCRYPT
+# crypt1 = [['g','o','n','e'],
+# 		  ['w','i','t','h'],
+# 		  ['t','h','e','w'],
+# 		  ['i','n','d','*']]
+
+crypt1 = [['t','h','e','c'],
+		  ['o','n','t','e'],
+		  ['s','t','i','s'],
+		  ['o','v','e','r']]
+for row in crypt1:
+	print(row)
+print('---------')
+crypt1_t = list(zip(*crypt1))
+for row in crypt1_t:
+	print(row)
+
+emptyStr = ''
+for row in crypt1_t:
+	rowStr = ''
+	for item in row:
+		if item != '*':
+			rowStr += item
+	emptyStr += rowStr[::-1]
+print(emptyStr)
+
+# for i in range(len(crypt1_t)-1,-1):
+# 	for j in range(len(crypt1_t)-1,-1):
+
+#
