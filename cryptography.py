aa48770ece4ff964d45ff59d0cce7935b3c28d88
Vinayak Sahal <32996115+vsahal@users.noreply.github.com>
2019-02-07 14:29:07 -0600

    Update cryptography.py
    added decrypt function and made matrices more ordered

--- cryptography.py
+++ cryptography.py
@@ -1,14 +1,15 @@
 
 #ENCRYPT
 # crypt1 = [['g','o','n','e'],
-# 		  ['w','i','t','h'],
-# 		  ['t','h','e','w'],
-# 		  ['i','n','d','*']]
+# 	    ['w','i','t','h'],
+# 	    ['t','h','e','w'],
+# 	    ['i','n','d','*']]
 
 crypt1 = [['t','h','e','c'],
-		  ['o','n','t','e'],
-		  ['s','t','i','s'],
-		  ['o','v','e','r']]
+	  ['o','n','t','e'],
+	  ['s','t','i','s'],
+          ['o','v','e','r']]
+
 for row in crypt1:
 	print(row)
 print('---------')
@@ -28,4 +29,21 @@ print(emptyStr)
 # for i in range(len(crypt1_t)-1,-1):
 # 	for j in range(len(crypt1_t)-1,-1):
 
-#
+#DECRYPT
+decrypt1 = [['o','s','o','t'],
+	    ['v','t','n','h'],
+	    ['e','i','t','e'],
+	    ['r','s','e','c']]
+
+decrypt_1 = list(zip(*decrypt1))
+for row in decrypt_1:
+	print(row)
+
+emptyStr = ''
+for row in decrypt_1:
+	rowStr = ''
+	for item in row:
+		if item != '*':
+			rowStr += item
+	emptyStr += rowStr[::-1]
+print(emptyStr[::-1])
