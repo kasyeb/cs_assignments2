145c033012250ba918ecfa8af238b581854bd79f
Vinayak Sahal <32996115+vsahal@users.noreply.github.com>
2019-02-07 19:32:50 -0600

    Update cryptography.py
    decrypt function isn't working but encrypt is good

--- cryptography.py
+++ cryptography.py
@@ -1,97 +1,129 @@
 
-#ENCRYPT
-# crypt1 = [['g','o','n','e'],
-# 	    ['w','i','t','h'],
-# 	    ['t','h','e','w'],
-# 	    ['i','n','d','*']]
 
-crypt1 = [['t','h','e','c'],
-	  ['o','n','t','e'],
-	  ['s','t','i','s'],
-          ['o','v','e','r']]
+def encrypt(strng):
 
-for row in crypt1:
-	print(row)
-print('---------')
-crypt1_t = list(zip(*crypt1))
-for row in crypt1_t:
-	print(row)
+    if len(strng) == 1:
+        return str
+    elif len(strng) <= 4:
+        matrix = [[None] * 2 for i in range(2)]
 
-emptyStr = ''
-for row in crypt1_t:
-	rowStr = ''
-	for item in row:
-		if item != '*':
-			rowStr += item
-	emptyStr += rowStr[::-1]
-print(emptyStr)
+    elif len(strng) <= 9:
+        matrix = [[None] * 3 for i in range(3)]
 
-# for i in range(len(crypt1_t)-1,-1):
-# 	for j in range(len(crypt1_t)-1,-1):
+    elif len(strng) <= 16:
+        matrix = [[None] * 4 for i in range(4)]
 
-#DECRYPT
-decrypt1 = [['o','s','o','t'],
-	    ['v','t','n','h'],
-	    ['e','i','t','e'],
-	    ['r','s','e','c']]
+    elif len(strng) <= 25:
+        matrix = [[None] * 5 for i in range(5)]
 
-decrypt_1 = list(zip(*decrypt1))
-for row in decrypt_1:
-	print(row)
+    elif len(strng) <= 36:
+        matrix = [[None] * 6 for i in range(6)]
 
-emptyStr = ''
-for row in decrypt_1:
-	rowStr = ''
-	for item in row:
-		if item != '*':
-			rowStr += item
-	emptyStr += rowStr[::-1]
-print(emptyStr[::-1])
+    elif len(strng) <= 49:
+        matrix = [[None] * 7 for i in range(7)]
 
-# Matrix maker
-def matrixMaker(str):
-  if len(str) == 1:
-    return str
-  if len(str) <= 4:
-    matrix = [[None] * 2 for i in range(2)]
-    return matrix
-  if len(str) <= 9:
-    matrix = [[None] * 3 for i in range(3)]
-    return matrix
-  if len(str) <= 16:
-    matrix = [[None] * 4 for i in range(4)]
-    return matrix
-  if len(str) <= 25:
-    matrix = [[None] * 5 for i in range(5)]
-    return matrix
-  if len(str) <= 36:
-    matrix = [[None] * 6 for i in range(6)]
-    return matrix
-  if len(str) <= 49:
-    matrix = [[None] * 7 for i in range(7)]
-    return matrix
-  if len(str) <= 64:
-    matrix = [[None] * 8 for i in range(8)]
-    return matrix
-  if len(str) <= 81:
-    matrix = [[None] * 9 for i in range(9)]
-    return matrix
-  if len(str) <= 100:
-    matrix = [[None] * 10 for i in range(10)]
-    return matrix
+    elif len(strng) <= 64:
+        matrix = [[None] * 8 for i in range(8)]
 
-# populating made matrix with letters from words
-matrix2 = [[None] * 4 for i in range(4)]
+    elif len(strng) <= 81:
+        matrix = [[None] * 9 for i in range(9)]
 
-word = 'thecontestisover'
-word1 = 'gonewiththewind'
+    elif len(strng) <= 100:
+        matrix = [[None] * 10 for i in range(10)]
 
-k = 0
-for i in range(len(matrix2)):
-	for j in range(len(matrix2)):
-		matrix2[i][j] = word[k] # works for words with length of a square number, else I get index error
-		k +=1 
+    k = 0
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            if k < len(strng):
+                matrix[i][j] = strng[k]
+                k += 1
+            else:
+                matrix[i][j] = '*'
 
-print(matrix2)
+    finalMatrix = list(zip(*matrix))
+
+    encryptStr = ''
+    for row in finalMatrix:
+        rowStr = ''
+        for item in row:
+            if item != '*':
+                rowStr += item
+        encryptStr += rowStr[::-1]
+
+    print(encryptStr)
 
 
+encrypt('thecontestisover')
+
+def decrypt(strng):
+
+    if len(strng) == 1:
+        return str
+    elif len(strng) <= 4:
+        matrix = [[None] * 2 for i in range(2)]
+
+    elif len(strng) <= 9:
+        matrix = [[None] * 3 for i in range(3)]
+
+    elif len(strng) <= 16:
+        matrix = [[None] * 4 for i in range(4)]
+
+    elif len(strng) <= 25:
+        matrix = [[None] * 5 for i in range(5)]
+
+    elif len(strng) <= 36:
+        matrix = [[None] * 6 for i in range(6)]
+
+    elif len(strng) <= 49:
+        matrix = [[None] * 7 for i in range(7)]
+
+    elif len(strng) <= 64:
+        matrix = [[None] * 8 for i in range(8)]
+
+    elif len(strng) <= 81:
+        matrix = [[None] * 9 for i in range(9)]
+
+    elif len(strng) <= 100:
+        matrix = [[None] * 10 for i in range(10)]
+
+    matrix1 = list(zip(*matrix))
+
+    k = 0
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            if k < len(strng):
+                matrix[i][j] = strng[k]
+                k +=1
+            else:
+                matrix[i][j] = '*'
+
+    finalMatrix = list(zip(*matrix))
+
+    decryptStr = ''
+    for row in finalMatrix:
+        rowStr = ''
+        for item in row:
+            if item != '*':
+                rowStr += item
+        decryptStr += rowStr[::-1]
+
+    print(decryptStr[::-1])
+
+decrypt('itwgnhiodetnwhe')
+
+
+
+# def decrypt(strng):
+#   #If len(str) < 2: return strng?
+#   pass
+
+# def main():
+#   fileEncrypt = open('encrypt.txt', 'r')
+#   n = int((fileEncrypt.readline()))
+#   for i in range(n):
+#       print(encrypt())
+
+#   fileDecrypt = open('decrypt.txt', 'r')
+#   m = int((fileDecrypt.readline()))
+#   for i in range(m):
+#       print(decrypt())
