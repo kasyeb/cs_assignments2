7cad81804ba924b9c6eb187b00245b117f1b7527
Vinayak Sahal <32996115+vsahal@users.noreply.github.com>
2019-02-14 18:47:32 -0600

    Update officespace.py
    added some comments

--- officespace.py
+++ officespace.py
@@ -22,11 +22,6 @@ def unallocatedArea():
     pass
     # return totalArea - usedArea
 
-def contestedArea():
-    pass
-    # 
-    # for i in range((num from txt file)):
-
 
 class Employee():
     def __init__(self, x1=0, y1=0, x2=0, y2=0):
@@ -36,8 +31,12 @@ class Employee():
         self.y2 = y2
         self.area = (x2-x1)*(y2-y1)
 
-    def contestedArea(self, e): #strictly looks at overlap, if touching edges then its false
+    def contestedSpace(self, e): #strictly looks at overlap, if touching edges then its false
         return not((self.x2 <= e.x1) or (self.y2 <= e.y1) or (self.x1 >= e.x2) or (self.y1 >= e.y2))
+    
+    def contestedSpaceArea():
+        pass
+         # it contestedArea True then find area of middle shit (overlapping of rectangles)
 
 
 
@@ -50,7 +49,7 @@ class Employee():
 def main():
     alice = Employee(2,3,10,11)
     ted = Employee(7,2,18,8)
-    print(alice.contestedArea(ted))
+    print(alice.contestedSpace(ted))
 
 
 
