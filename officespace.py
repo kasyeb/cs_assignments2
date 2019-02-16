2fbb300e9270e9e220595b1749c4cdf55d97df69
Vinayak Sahal <32996115+vsahal@users.noreply.github.com>
2019-02-15 19:38:41 -0600

    Update officespace.py
    worked on it some more

--- officespace.py
+++ officespace.py
@@ -1,6 +1,6 @@
 #  File: OfficeSpace.py
 
-#  Description: Program that identifies overlapping spaces between employees and what empty spaces  
+#  Description: Program that identifies overlapping spaces between employees and what empty spaces
 
 #  Student Name: Vinayak Sahal
 
@@ -14,9 +14,12 @@
 
 #  Date Last Modified:
 
+import math
+
 
 def totalArea(l, w):
-    return l * w 
+    return l * w
+
 
 def unallocatedArea():
     pass
@@ -29,28 +32,64 @@ class Employee():
         self.y1 = y1
         self.x2 = x2
         self.y2 = y2
-        self.area = (x2-x1)*(y2-y1)
+        self.area = (x2 - x1) * (y2 - y1)
 
-    def contestedSpace(self, e): #strictly looks at overlap, if touching edges then its false
+    def contestedSpace(self, e):  # strictly looks at overlap, if touching edges then its false
         return not((self.x2 <= e.x1) or (self.y2 <= e.y1) or (self.x1 >= e.x2) or (self.y1 >= e.y2))
-    
-    def contestedSpaceArea():
-        pass
-         # it contestedArea True then find area of middle shit (overlapping of rectangles)
+
+    def contestedSpaceArea(self, e):
+        a1 = abs(self.x1 - self.x2) * abs(self.y1 - self.y2)
+        a2 = abs(e.x1 - e.x2) * abs(e.y1 - e.y2)
+
+        intersectingArea = (min(self.x2, e.x2) - max(self.x1, e.x1)) * (min(self.y2, e.y2) - max(self.y1, e.y1))
+
+        return (intersectingArea)
 
 
-
-
-
-
-
+def getTotalContested(empList, newEmployee):
+    contestedArea = 0
+    for employee in empList:
+        if employee.contestedSpace(newEmployee):
+            employee.area -= employee.contestedSpaceArea(newEmployee)
+            contestedArea += employee.contestedSpaceArea(newEmployee)
+    print(contestedArea)
+    return contestedArea
 
 
 def main():
-    alice = Employee(2,3,10,11)
-    ted = Employee(7,2,18,8)
-    print(alice.contestedSpace(ted))
 
+    with open('office.txt')
+    file = open('office.txt', 'r')
+    line1 = file.readline()
+    officeLength = int(line1.split(' ')[0])
+    officeWidth = int(line1.split(' ')[1])
+    print('Total', totalArea(officeLength, officeWidth))
+
+    empList = []
+    numEmployees = int((file.readline()))
+
+    for i in range(numEmployees):
+        line3 = file.readline()
+        employee = line3.split(' ')[0]
+        x1 = line3.split(' ')[1]
+        y1 = line3.split(' ')[2]
+        x2 = line3.split(' ')[3]
+        y2 = line3.split(' ')[4]
+        newEmployee = Employee(x1,y1,x2,y2)
+
+        if len(empList) < 1:
+            empList.append()
+
+
+
+    
+    alice = Employee(2, 3, 10, 11)
+    empList.append(alice)
+    ted = Employee(7, 2, 18, 8)
+    greedybob = Employee(17, 11, 30, 24)
+    empList.append(greedybob)
+    print(getTotalContested(empList, ted))
+    empList.append(ted)
 
 
 main()
