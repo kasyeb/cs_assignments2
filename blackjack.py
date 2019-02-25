6893b4f975eae8b2d43f915f39d4815c10cd5cd6
Vinayak Sahal <32996115+vsahal@users.noreply.github.com>
2019-02-24 20:19:59 -0600

    Create blackjack.py
    started new assignment

---
+++ blackjack.py
@@ -0,0 +1,15 @@
+# File: Blackjack.py
+
+# Description: Program that allows the user to play blackjack
+
+# Student's Name: Vinayak Sahal
+
+# Student's UT EID: vs9736
+
+# Course Name: CS 313E
+
+# Unique Number: 50725
+
+# Date Created: 2/24/19
+
+# Date Last Modified:
