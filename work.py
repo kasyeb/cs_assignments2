ec6a7614e181c2b6a206ae0191256666b40385cc
Vinayak Sahal <32996115+vsahal@users.noreply.github.com>
2019-03-04 16:02:41 -0600

    Update work.py
    added comments

--- work.py
+++ work.py
@@ -12,13 +12,14 @@
 
 #  Date Created: 2/4/2019
 
-#  Date Last Modified: 2/4/2019
+#  Date Last Modified:
 
-
+# binary search which finds v by calling linesCode function
 def binSearch(n, k):
   low = 1
   high = n
 
+# simple structure like binary code
   while (low <= high):
     middle = (low + high) // 2
     if linesCode(n, k, middle) == False:
@@ -31,7 +32,7 @@ def binSearch(n, k):
 
   return v 
 
-
+# goes through values while 2 conditions are not met
 def linesCode(n, k, middle):
   start = 1
   count = middle
@@ -40,13 +41,14 @@ def linesCode(n, k, middle):
     count += (middle // (k ** start))
     start += 1
 
+# result here is returned back to the binSearch function
   if count >= n:
     return True
 
   else:
     return False
 
-
+# main parses text file and assigns n and k to binsearch
 def main():
   file = open('work.txt', 'r')
   numTestCases = int(file.readline())
