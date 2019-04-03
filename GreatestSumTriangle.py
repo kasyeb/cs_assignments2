b34b1e5284555d050724dec078065a011c99d0c3
Vinayak Sahal <32996115+vsahal@users.noreply.github.com>
2019-04-03 13:59:59 -0500

    Create GreatestSumTriangle.py
    starting new assignment

---
+++ GreatestSumTriangle.py
@@ -0,0 +1,47 @@
+from timeit import timeit
+
+# returns the greatest path sum using exhaustive search
+def brute_force (grid):
+  return
+
+# returns the greatest path sum using greedy approach
+def greedy (grid):
+  return
+
+# returns the greatest path sum using divide and conquer (recursive) approach
+def divide_conquer (grid):
+  return
+
+# returns the greatest path sum and the new grid using dynamic programming
+def dynamic_prog (grid):
+  return
+
+# reads the file and returns a 2-D list that represents the triangle
+def read_file ():
+  return 
+
+def main ():
+  # read triangular grid from file
+
+  # output greatest path from exhaustive search
+  times = timeit ('brute_force({})'.format(grid)), 'from __main__ import brute_force', number = 10)
+  times = times / 10
+  # print time taken using exhaustive search
+
+  # output greatest path from greedy approach
+  times = timeit ('greedy({})'.format(grid), 'from __main__ import greedy', number = 10)
+  times = times / 10
+  # print time taken using greedy approach
+
+  # output greatest path from divide-and-conquer approach
+  times = timeit ('divide_conquer({})'.format(grid), 'from __main__ import divide_conquer', number = 10)
+  times = times / 10
+  # print time taken using divide-and-conquer approach
+
+  # output greatest path from dynamic programming 
+  times = timeit ('dynamic_prog({})'.format(grid), 'from __main__ import dynamic_prog', number = 10)
+  times = times / 10
+  # print time taken using dynamic programming
+
+if __name__ == "__main__":
+  main()
