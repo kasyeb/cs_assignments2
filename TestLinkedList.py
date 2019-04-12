6f3312f31fa181220b81f4e14cf8ff30c18b899d
Vinayak Sahal <32996115+vsahal@users.noreply.github.com>
2019-04-11 20:01:46 -0500

    Update TestLinkedList.py
    updated functions

--- TestLinkedList.py
+++ TestLinkedList.py
@@ -15,59 +15,111 @@
 #  Date Last Modified:
 
 
-# LOOK AT MITRA NOTES
-class Link (object):
-  ...
+class Link(object):
+  def __init__(self, data, next=None):
+    self.data = data
+    self.next = next
+
 
 class LinkedList (object):
-  # get number of links 
-  def get_num_links (self):
-  
-  # add an item at the beginning of the list
-  def insert_first (self, data): 
+  def __init__(self):
+    self.first = None
 
-  # add an item at the end of a list
-  def insert_last (self, data): 
+  # get number of links
+  def get_num_links(self):
 
-  # add an item in an ordered list in ascending order
-  def insert_in_order (self, data): 
+    # add an item at the beginning of the list
+  def insert_first(self, data):
+    new_link = Link(data)
+    new_link.next = self.first
+    self.first = new_link
 
-  # search in an unordered list, return None if not found
-  def find_unordered (self, data): 
+    # add an item at the end of a list
+  def insert_last(self, data):
+    new_link = Link(data)
+    # find the last link
+    current = self.first
+    if (current == None):
+      self.first = new_link
+      return
+    while (current.next != None):
+      current = current.next
 
-  # Search in an ordered list, return None if not found
-  def find_ordered (self, data): 
+    current.next = new_link
 
-  # Delete and return Link from an unordered list or None if not found
-  def delete_link (self, data):
+    # add an item in an ordered list in ascending order
+  def insert_in_order(self, data):
 
-  # String representation of data 10 items to a line, 2 spaces between data
-  def __str__ (self):
+    # search in an unordered list, return None if not found
+  def find_unordered(self, data):
 
-  # Copy the contents of a list and return new list
-  def copy_list (self):
+    # Search in an ordered list, return None if not found
+  def find_ordered(self, data):
 
-  # Reverse the contents of a list and return new list
-  def reverse_list (self): 
+  def find_link(self, data):
+    current = self.first
 
-  # Sort the contents of a list in ascending order and return new list
-  def sort_list (self): 
+    if (current == None):
+      return None
 
-  # Return True if a list is sorted in ascending order or False otherwise
-  def is_sorted (self):
+    while (current.data != data):
+      if (current.next == None):
+        return None
+      else:
+        current = current.next
 
-  # Return True if a list is empty or False otherwise
-  def is_empty (self): 
+    return current
 
-  # Merge two sorted lists and return new list in ascending order
-  def merge_list (self, other): 
+    # Delete and return Link from an unordered list or None if not found
+  def delete_link(self, data):
+    previous = self.first
+    current = self.first
 
-  # Test if two lists are equal, item by item and return True
-  def is_equal (self, other):
+    if (current == None):
+      return None
 
-  # Return a new list, keeping only the first occurence of an element
-  # and removing all duplicates. Do not change the order of the elements.
-  def remove_duplicates (self):
+    while (current.data != data):
+      if (current.next == None):
+        return None
+      else:
+        previous = current
+        current = current.next
+
+      if (current == self.first):
+        self.first = self.first.next
+      else:
+        previous.next = current.next
+
+    return current
+
+    # String representation of data 10 items to a line, 2 spaces between data
+  def __str__(self):
+
+    # Copy the contents of a list and return new list
+  def copy_list(self):
+
+    # Reverse the contents of a list and return new list
+  def reverse_list(self):
+
+    # Sort the contents of a list in ascending order and return new list
+  def sort_list(self):
+
+    # Return True if a list is sorted in ascending order or False otherwise
+  def is_sorted(self):
+
+    # Return True if a list is empty or False otherwise
+  def is_empty(self):
+
+    # Merge two sorted lists and return new list in ascending order
+  def merge_list(self, other):
+
+    # Test if two lists are equal, item by item and return True
+  def is_equal(self, other):
+
+    # Return a new list, keeping only the first occurence of an element
+    # and removing all duplicates. Do not change the order of the elements.
+  def remove_duplicates(self):
+
 
 def main():
   # Test methods insert_first() and __str__() by adding more than
@@ -79,14 +131,14 @@ def main():
 
   # Test method get_num_links()
 
-  # Test method find_unordered() 
-  # Consider two cases - data is there, data is not there 
+  # Test method find_unordered()
+  # Consider two cases - data is there, data is not there
 
-  # Test method find_ordered() 
-  # Consider two cases - data is there, data is not there 
+  # Test method find_ordered()
+  # Consider two cases - data is there, data is not there
 
   # Test method delete_link()
-  # Consider two cases - data is there, data is not there 
+  # Consider two cases - data is there, data is not there
 
   # Test method copy_list()
 
@@ -105,6 +157,7 @@ def main():
   # Consider two cases - lists are equal, lists are not equal
 
   # Test remove_duplicates()
+
 
 if __name__ == "__main__":
   main()
