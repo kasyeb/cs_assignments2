ff8a9ed6de24ebb9e0e8f31dc98253ac346d63e1
Vinayak Sahal <32996115+vsahal@users.noreply.github.com>
2019-04-16 20:10:17 -0500

    Create ExpressionTree.py
    new assignment

---
+++ ExpressionTree.py
@@ -0,0 +1,35 @@
+#  File: ExpressionTree.py
+
+#  Description:
+
+#  Student's Name:
+
+#  Student's UT EID:
+
+#  Course Name: CS 313E 
+
+#  Unique Number: 
+
+#  Date Created:
+
+#  Date Last Modified:
+
+
+class Stack (object):
+
+class Node (object):
+
+class Tree (object):
+  def __init__ (self):
+
+  def create_tree (self, expr):
+
+  def evaluate (self, aNode):
+
+  def pre_order (self, aNode):
+
+  def post_order (self, aNode):
+
+def main():
+
+main()
