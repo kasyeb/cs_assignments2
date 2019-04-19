3ce0baba9c07ffc9032d349ed8ffb72a8a697f31
Vinayak Sahal <32996115+vsahal@users.noreply.github.com>
2019-04-18 20:43:21 -0500

    Update ExpressionTree.py
    worked on eval func

--- ExpressionTree.py
+++ ExpressionTree.py
@@ -1,51 +1,72 @@
 #  File: ExpressionTree.py
 
-#  Description: Solving questions three different ways.
+#  Description: Using tree data strucutre to solve arthicmatic problems
 
 #  Student's Name: Vinayak Sahal
 
 #  Student's UT EID: vs9736
 
-#  Course Name: CS 313E 
+#  Course Name: CS 313E
 
-#  Unique Number:50725
+#  Unique Number: 50725
 
 #  Date Created: 4/16/2019
 
 #  Date Last Modified:
 
+
 class Stack (object):
-  def __init__ (self):
+  def __init__(self):
     self.stack = []
-  
-  def push (self, item):
+
+  def push(self, item):
     self.stack.append(item)
-  
-  def pop (self):
+
+  def pop(self):
     return self.stack.pop()
-  
+
   def peek(self):
     return self.stack[-1]
- 
-  def is_empty(self);
-    return (len(self.stack) == 0)
-  
-  def size (self):
+
+  def is_empty(self):
+    return len(self.stack == 0)
+
+  def size(self):
     return (len(self.stack))
-  
+
+
 class Node (object):
+  def __init__(self, data):
+    self.data = data
+    self.leftChild = None
+    self.rightChild = None
+
 
 class Tree (object):
-  def __init__ (self):
+  def __init__(self):
+    self.root = None # Node(None)
 
-  def create_tree (self, expr):
+  def create_tree(self, expr):
 
-  def evaluate (self, aNode):
+  def evaluate(self, aNode):
+    if (aNode.data == '+'):
+      return (self.evaluate(aNode, leftChild) + self.evaluate(aNode, rightChild))
 
-  def pre_order (self, aNode):
+    elif (aNode.data == '-')
+      return (self.evaluate(aNode, leftChild) - self.evaluate(aNode, rightChild))
 
-  def post_order (self, aNode):
+    elif (aNode.data == '*')
+      return (self.evaluate(aNode, leftChild) * self.evaluate(aNode, rightChild))
+
+    elif (aNode.data == '/')
+      return (self.evaluate(aNode, leftChild) / self.evaluate(aNode, rightChild))
+
+  def pre_order(self, aNode):
+
+  def post_order(self, aNode):
+
 
 def main():
+
 
 main()
